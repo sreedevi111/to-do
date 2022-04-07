12d951807a1265a126e320110f83e82f8c2a0342
sreedevi111 <sreedevisobhana@gmail.com>
2022-04-07 18:41:08 +0530

    cleared unwanted files and updated app.js and css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+
+
+
 function App() {
+ const[toDos, setToDos] = useState([]);
+ const[toDo, setToDo] =useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input value = {toDo} onChange = {(e) => setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>setToDo([...toDos,toDo])} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        <div className="todo">
+          <div className="left">
+            <input type="checkbox" name="" id="" />
+            <p>Rect tutorial</p>
+          </div>
+          <div className="right">
+            <i className="fas fa-times"></i>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
